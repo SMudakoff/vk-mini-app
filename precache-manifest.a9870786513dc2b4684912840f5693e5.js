6a7bc2125b07ece4ab2b2121fa272677c31b21ae
Ushakov Sergey <serega_twitch@mail.ru>
2021-02-25 21:55:26 +0300

    Updates

---
+++ precache-manifest.a9870786513dc2b4684912840f5693e5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "326b3921e00c5946519b2d377272a8eb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4f73c24c3b0c5f281228",
+    "url": "./static/css/2.cdee455b.chunk.css"
+  },
+  {
+    "revision": "5629f51f87f678d69e93",
+    "url": "./static/css/main.577355a6.chunk.css"
+  },
+  {
+    "revision": "4f73c24c3b0c5f281228",
+    "url": "./static/js/2.ef894ea8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ef894ea8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5629f51f87f678d69e93",
+    "url": "./static/js/main.b224b647.chunk.js"
+  },
+  {
+    "revision": "556157b2a190f5096a33",
+    "url": "./static/js/runtime-main.74210683.js"
+  },
+  {
+    "revision": "c67f4e277aef1a840c759173d2885079",
+    "url": "./static/media/group_1.c67f4e27.jpg"
+  },
+  {
+    "revision": "ed1618a738445eff00f0845babbe2598",
+    "url": "./static/media/oleg_3.ed1618a7.png"
+  },
+  {
+    "revision": "61babf53ce0143c32dbc73462ed4cbca",
+    "url": "./static/media/schedule.61babf53.png"
+  }
+]);
